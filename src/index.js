69decfe2ceb7cda226c9bd11be4a49901ec243fe
Guilherme <guilhherme10@outlook.com>
2023-08-30 20:53:01 -0300

    correçao bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const knex = require("./conexao");
+// const knex = require("./conexao");
 const app = express();
 app.use(express.json());
 
